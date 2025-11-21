ee4aaf1fe7cba8335f8acf873f5745fc381819fc
Steel Maxima <steelmaxima21@gmail.com>
2025-11-21 07:31:33 +0100

    fix: add migration file for add_user

---
+++ prisma/migrations/20251120135641_add_avatars/nigration.sql
@@ -0,0 +1,52 @@
+/*
+  Warnings:
+  - You are about to drop the column `avatarUrl` on the `kids` table. All the data in the column will be lost.
+  - You are about to drop the column `avatarUrl` on the `users` table. All the data in the column will be lost.
+*/
+-- AlterTable
+ALTER TABLE "_StoryCategories" ADD CONSTRAINT "_StoryCategories_AB_pkey" PRIMARY KEY ("A", "B");
+
+-- DropIndex
+DROP INDEX "_StoryCategories_AB_unique";
+
+-- AlterTable
+ALTER TABLE "_StoryThemes" ADD CONSTRAINT "_StoryThemes_AB_pkey" PRIMARY KEY ("A", "B");
+
+-- DropIndex
+DROP INDEX "_StoryThemes_AB_unique";
+
+-- AlterTable
+ALTER TABLE "kids" DROP COLUMN "avatarUrl",
+ADD COLUMN     "avatarId" TEXT;
+
+-- AlterTable
+ALTER TABLE "users" DROP COLUMN "avatarUrl",
+ADD COLUMN     "avatarId" TEXT;
+
+-- CreateTable
+CREATE TABLE "avatars" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "displayName" TEXT,
+    "url" TEXT NOT NULL,
+    "isSystemAvatar" BOOLEAN NOT NULL DEFAULT false,
+    "publicId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "avatars_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "avatars_name_idx" ON "avatars"("name");
+
+-- CreateIndex
+CREATE INDEX "avatars_isSystemAvatar_idx" ON "avatars"("isSystemAvatar");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "avatars_name_key" ON "avatars"("name");
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_avatarId_fkey" FOREIGN KEY ("avatarId") REFERENCES "avatars"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "kids" ADD CONSTRAINT "kids_avatarId_fkey" FOREIGN KEY ("avatarId") REFERENCES "avatars"("id") ON DELETE SET NULL ON UPDATE CASCADE;
