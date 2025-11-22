424ff86e66d9562fe40c14f59dd87264ef5b00f0
luli-tech <olabodemicheal5@gmail.com>
2025-11-22 15:54:29 +0100

    fix(age):deleted and regenerated age database migration

--- prisma/migrations/20251122012325_create_age_group_age/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `updatedAt` on the `age_groups` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "age_groups" DROP COLUMN "updatedAt";
