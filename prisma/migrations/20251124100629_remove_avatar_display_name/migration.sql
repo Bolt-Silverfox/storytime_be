360781ff4b67218002739b085f89c11b2aadfa68
Kessi-ux <chukwubueze.uchenyi.231361@unn.edu.ng>
2025-11-26 10:53:41 +0100

    feature:activity-logs

--- prisma/migrations/20251124100629_remove_avatar_display_name/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `displayName` on the `avatars` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "avatars" DROP COLUMN "displayName";
