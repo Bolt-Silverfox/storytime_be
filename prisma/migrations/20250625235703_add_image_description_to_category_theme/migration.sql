4d40baf61889a015ba931c521d99d402aa46ae9c
Aghaulor-Gift <132744631+Aghaulor-Gift@users.noreply.github.com>
2025-11-26 20:16:24 +0100

    Sync prisma folder from develop-v0.0.1

---
+++ prisma/migrations/20250625235703_add_image_description_to_category_theme/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "Category" ADD COLUMN     "description" TEXT,
+ADD COLUMN     "image" TEXT;
+
+-- AlterTable
+ALTER TABLE "Theme" ADD COLUMN     "description" TEXT,
+ADD COLUMN     "image" TEXT;
