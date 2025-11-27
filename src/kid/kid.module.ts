8f1402717a860aa041412ff261003a0283c1b49f
Oluwatise Ajayi <dr393462@gmail.com>
2025-11-27 13:58:43 +0100

    feat: Implement kid and voice management services, including ElevenLabs integration for kid profiles.

--- src/kid/kid.module.ts
+++ src/kid/kid.module.ts
@@ -3,9 +3,10 @@ import { KidController } from './kid.controller';
 import { KidService } from './kid.service';
 import { PrismaService } from '../prisma/prisma.service';
 import { AuthModule } from '../auth/auth.module';
+import { VoiceModule } from '../voice/voice.module';
 
 @Module({
-    imports: [AuthModule],
+    imports: [AuthModule, VoiceModule],
     controllers: [KidController],
     providers: [KidService, PrismaService],
     exports: [KidService],
