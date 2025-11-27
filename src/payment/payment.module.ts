623ec6c770c80f3a68fb1d7f5eb55a85b951abee
Aghaulor-Gift <132744631+Aghaulor-Gift@users.noreply.github.com>
2025-11-27 16:38:50 +0100

    fix:payment and subscription module

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -1,8 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [
+    forwardRef(() => AuthModule), 
+  ],
   providers: [PaymentService],
   controllers: [PaymentController],
   exports: [PaymentService],
--- src/subscription/subscription.module.ts
+++ src/subscription/subscription.module.ts
@@ -1,11 +1,16 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { SubscriptionService } from './subscription.service';
 import { SubscriptionController } from './subscription.controller';
-import { PaymentModule } from '../payment/payment.module'; 
+import { PaymentModule } from '../payment/payment.module';
 import { UserModule } from '../user/user.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [forwardRef(() => PaymentModule), forwardRef(() => UserModule)],
+  imports: [
+    forwardRef(() => PaymentModule),
+    forwardRef(() => UserModule),
+    forwardRef(() => AuthModule),
+  ],
   providers: [SubscriptionService],
   controllers: [SubscriptionController],
   exports: [SubscriptionService],
