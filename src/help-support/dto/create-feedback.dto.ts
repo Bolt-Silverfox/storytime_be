591723cecd4adb82a6ed25495131084378bf82ed
Morebytheway <moregracebytheway@gmail.com>
2025-11-29 10:26:18 +0100

    help-support

---
+++ src/help-support/dto/create-feedback.dto.ts
@@ -0,0 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateFeedbackDto {
+  @ApiProperty({ example: 'John Doe' })
+  @IsNotEmpty()
+  @IsString()
+  fullname: string;
+
+  @ApiProperty({ example: 'john@example.com' })
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({ example: 'I love the app! But add dark mode.' })
+  @IsNotEmpty()
+  @IsString()
+  message: string;
+}
