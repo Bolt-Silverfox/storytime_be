02c8f8698267cbffa2e632009c2d59e843d18380
Morebytheway <moregracebytheway@gmail.com>
2025-11-29 10:26:18 +0100

    help-support

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,20 +6,21 @@ import { AuthModule } from './auth/auth.module';
 import { AvatarModule } from './avatar/avatar.module';
 import { CommonModule } from './common/common.module';
 import { validateEnv } from './config/env.validation';
+import { HelpSupportModule } from './help-support/help-support.module';
+import { KidModule } from './kid/kid.module';
 import { NotificationModule } from './notification/notification.module';
+import { PaymentModule } from './payment/payment.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ReportsModule } from './reports/reports.module';
 import { RewardModule } from './reward/reward.module';
 import { SettingsModule } from './settings/settings.module';
+import { StoryBuddyModule } from './story-buddy/story-buddy.module';
 import { StoryModule } from './story/story.module';
+import { SubscriptionModule } from './subscription/subscription.module';
 import { CloudinaryModule } from './upload/cloudinary.module';
 import { UploadModule } from './upload/upload.module';
 import { UserModule } from './user/user.module';
-import { KidModule } from './kid/kid.module';
 import { VoiceModule } from './voice/voice.module';
-import { SubscriptionModule } from './subscription/subscription.module';
-import { PaymentModule } from './payment/payment.module';
-import { StoryBuddyModule } from './story-buddy/story-buddy.module';
 
 @Module({
   imports: [
@@ -48,6 +49,7 @@ import { StoryBuddyModule } from './story-buddy/story-buddy.module';
     SubscriptionModule,
     PaymentModule,
     StoryBuddyModule,
+    HelpSupportModule,
   ],
 })
-export class AppModule { }
+export class AppModule {}
