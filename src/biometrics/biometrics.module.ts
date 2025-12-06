8b3250b35fee14be5a57aa97b19670a26deb67e2
Michelle Ndiangui <ndianguimichelle@gmail.com>
2025-12-06 17:44:47 +0300

    refact: updating seeding

--- src/biometrics/biometrics.module.ts
+++ src/biometrics/biometrics.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BiometricsService } from './biometrics.service';
 import { BiometricsController } from './biometrics.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [BiometricsController],
   providers: [BiometricsService],
   exports: [BiometricsService],
 })
-export class BiometricsModule {}
+export class BiometricsModule { }
