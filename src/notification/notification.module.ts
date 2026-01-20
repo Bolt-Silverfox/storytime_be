5dcf838c4a7aff5bd6d1bda2d235034de8d5cca3
zxenonx <xenon.nemrod@gmail.com>
2026-01-20 21:28:36 +0100

    refactor(notification): integrate provider pattern
    - Updated NotificationService to use Channel Providers (Email, InApp)
    - Added sendViaProvider method for multi-channel dispatch
    - Registered providers in NotificationModule

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
+import { InAppProvider } from './providers/in-app.provider';
+import { EmailProvider } from './providers/email.provider';
 
 @Module({
   imports: [HttpModule],
   controllers: [NotificationController],
-  providers: [NotificationService],
+  providers: [NotificationService, InAppProvider, EmailProvider],
   exports: [NotificationService],
 })
-export class NotificationModule {}
+export class NotificationModule { }
