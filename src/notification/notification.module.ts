f24b562cefcdd3e3a695fae9b69918f34c50026c
zxenonx <xenon.nemrod@gmail.com>
2026-01-21 08:19:51 +0100

    feat(notifications): register in-app notification controller
    - Add InAppNotificationController to module controllers

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
+import { InAppNotificationController } from './in-app-notification.controller';
 import { InAppProvider } from './providers/in-app.provider';
 import { EmailProvider } from './providers/email.provider';
 
 @Module({
   imports: [HttpModule],
-  controllers: [NotificationController],
+  controllers: [NotificationController, InAppNotificationController],
   providers: [NotificationService, InAppProvider, EmailProvider],
   exports: [NotificationService],
 })
