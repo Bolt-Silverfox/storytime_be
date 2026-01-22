432066ebf40b2798df81a87e2a94f259298bb6ae
zxenonx <xenon.nemrod@gmail.com>
2026-01-22 23:29:28 +0100

    fix(deps): resolve circular dependency between AuthModule and NotificationModule using forwardRef

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,13 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
 import { InAppNotificationController } from './in-app-notification.controller';
 import { InAppProvider } from './providers/in-app.provider';
 import { EmailProvider } from './providers/email.provider';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, forwardRef(() => AuthModule)],
   controllers: [NotificationController, InAppNotificationController],
   providers: [NotificationService, InAppProvider, EmailProvider],
   exports: [NotificationService],
